7183d291aefdda059918c54ba91606da6b827867
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2021-10-03 10:44:27 -0600

    making txtToString Method

--- src/writer/model/FRCWriter.java
+++ src/writer/model/FRCWriter.java
@@ -23,15 +23,24 @@ public class FRCWriter
 	}
 	public ArrayList <String> canMotor(int canNumber, String canName)
 	{
+		String subName = canName + "Sub";
+		
 		ArrayList <String> canMotorData = new ArrayList <String>();
 		String canSub = "// Copyright (c) FIRST and other WPILib contributors. \n";
 		canSub += "// Open Source Software; you can modify and/or share it under the terms of \n";
 		canSub += "// the WPILib BSD license file in the root directory of this project. \n";
+		canSub += "\n";
 		canSub += "import edu.wpi.first.wpilibj.Victor; \n";
 		canSub += "import edu.wpi.first.wpilibj2.command.SubsystemBase; \n";
 		canSub += "import frc.robot.Constants; \n";
 		canSub += "import com.ctre.phoenix.motorcontrol.*; \n";
-		canSub+= "\n";
+		canSub += "\n";
+		canSub += "public class " + subName + "Sub extends SubsystemBase { \n";
+		canSub += "    /**  Creates a new " + subName + ". */ \n";
+		for (int i = 0; i < canNumber; i++) 
+		{
+			canSub += "    private TalonFX " + canName + i + "\n";  
+		}
 				
 		canMotorData.add(canSub);
 		
