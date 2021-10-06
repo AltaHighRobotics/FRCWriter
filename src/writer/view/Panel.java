0806dff5bdcf1729c7d6c91d14d3bc97f008c247
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2021-10-06 17:18:53 -0600

    made basic gui

--- src/writer/view/Panel.java
+++ src/writer/view/Panel.java
@@ -1,8 +1,7 @@
 package writer.view;
 
 import writer.controller.*;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Color;
 
@@ -10,12 +9,19 @@ public class Panel extends JPanel
 {
 	private Controller controller;
 	private JButton runButton;
-
+	private JComboBox <String> objectType; 
+	private String [] objectList = {"Can Motor","Rio Motor", "Pneumatic"};  
+	private SpinnerModel objectNumber = new SpinnerNumberModel (1,1,15,1);
+	private JTextField objectName;
+	
 	Panel(Controller controller)
 	{
 		super();
+		
 		this.controller = controller;
 		this.runButton = new JButton("Generate Code");
+		this.objectType = new JComboBox<String>(objectList);
+		this.objectName = new JTextField ("Function", 10);
 		
 		setupPanel();
 		setUpListeners();
@@ -23,10 +29,15 @@ public class Panel extends JPanel
 	
 	private void setupPanel()
 	{
+		JSpinner spinner = new JSpinner(objectNumber);
+		
 		this.setBackground(Color.DARK_GRAY);
 		this.setSize(800, 600);
-		
+		this.add(objectType);
+		this.add(spinner);
+		this.add(objectName);
 		this.add(runButton);
+		
 	}
 	
 	private void setUpListeners()
